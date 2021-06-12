0f35e475e190dd77d57f24cf03998ff306b2a763
Ashraf Shaker <sayedashrafshaker@yahoo.com>
2021-06-12 12:26:07 +0200

    Add customDate pipe

--- src/app/Pipe/custom-date.pipe.ts
+++ src/app/Pipe/custom-date.pipe.ts
@@ -8,31 +8,46 @@ export class CustomDatePipe implements PipeTransform {
   transform(value: Date): string {
     let date = new Date(value);
     const now = new Date();
-    let diff = new Date(now.valueOf() - date.valueOf())
-    console.log(now.valueOf() - date.valueOf())
-    let year = diff.getFullYear()
-    let month = diff.getMonth() 
-    let day = diff.getDay() 
-    let hour = diff.getHours() 
+    let seconds = (now.valueOf() - date.valueOf()) / 1000
+
+    var numyears = Math.floor(seconds / 31536000);
+    var numdays = Math.floor((seconds % 31536000) / 86400);
+    var numMonths = numdays / 30 | 0;
+    numdays = numdays % 30 | 0;
+
+    var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+    var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+    var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+
 
     let start = "منذ "
     let days = ["", "يوم", "يومين"]
-    let day2 = day < 3 ? days[day] : day + " ايام "
+    let day2 = numdays < 3 ? days[numdays] : numdays + " ايام "
     let years = ["", "سنة", "سنتين"]
-    let year2 = year < 3 ? years[year] : year + " سنوات "
+    let year2 = numyears < 3 ? years[numyears] : numyears + " سنوات "
     let hours = ["", "ساعة", "ساعتين"]
-    let hour2 = hour < 3 ? hours[hour] : hour + " ساعات"
+    let hour2 = numhours < 3 ? hours[numhours] : numhours + " ساعات"
     let months = ["", "شهر", "شهرين"]
-    let month2 = month < 3 ? months[month] : month + " أشهر"
+    let month2 = numMonths < 3 ? months[numMonths] : numMonths + " أشهر"
+    let minutes = ["", "دقيقة", "دقيقتين"]
+    let minutes2 = numminutes < 3 ? minutes[numminutes] : numminutes + " دقائق"
 
-    if (year2 + month2 + day2 == "") {
-      return `${start} ${hour2}`
+    if (numminutes > 10) minutes2 = numminutes + " دقيقة";
+    if (numdays > 10) day2 = numdays + " يوم";
+    if (numhours > 10) hour2 = numhours + " ساعة";
+    if (numyears > 10) year2 = numyears + " سنة";
+    if (numMonths > 10) month2 = numMonths + " شهر";
+
+
+    if (year2 + month2 + day2 + hour2 == "") {
+      return `${start} ${minutes2}`
+    } else if (year2 + month2 + day2 == "") {
+      return `${start} ${hour2} و${minutes2}`
     } else if (year2 + month2 == "") {
-      return `${start} ${day2} و${hour2}`
-    } else if (year2  == "") {
+      return `${start} ${day2} و${hour2} `
+    } else if (year2 == "") {
       return `${start} ${month2} و${day2}`
-    }else
-    {
+    } else {
       return `${start} ${year2} و${month2}`
 
     }
