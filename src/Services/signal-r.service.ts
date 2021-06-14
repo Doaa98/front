ae8d256c716c70910222cbd49acfc46196d27e50
Amr AbdelBasit <amrabdelbaset50@yahoo.com>
2021-06-14 22:24:45 +0200

    Merge

--- src/Services/signal-r.service.ts
+++ src/Services/signal-r.service.ts
@@ -3,7 +3,7 @@ import * as signalR from "@aspnet/signalr";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Notification } from 'src/app/Shared/notification';
+import { Notification } from 'src/app/models/notification';
 
 
 @Injectable({
